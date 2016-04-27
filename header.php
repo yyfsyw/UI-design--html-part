14305f9875048d8295b118a97de71469598bfa88
yunfei Yang <yyf250system@hotmail.com>
2016-04-26 22:24:26 -0500

    Added files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<!--      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">-->
+<!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>-->
+<!--        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
+</head>
+
+    <div class="container" style="height: 70px;">
+        <span ><a href="index.php"><img src="img/logo.png" alt="Goody"></a> </span> <span style="font-family: sans-serif;font-size: 20px;"> Exchange Every Thing Here</span>
+        
+        <a href="signin.php" style="margin-left: 40%" class="btn btn-info"><i class="glyphicon glyphicon-user"></i> My Account</a> <a href="submitad.php" style="margin-left: 0"   class="btn btn-success"><i class="glyphicon glyphicon-pencil"></i> Submit A Free Ad</a>
+    </div>
+</html>
