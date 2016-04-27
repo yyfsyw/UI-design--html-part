e39efeb21eb244cf6360223bebac706545e939b8
yunfei Yang <yyf250system@hotmail.com>
2016-04-26 20:32:59 -0500

    Added files via upload

--- signin.php
+++ signin.php
@@ -1,3 +1,10 @@
+<?php
+// Start the session
+session_start();
+if(isset($_SESSION['email'])){
+    header("Location: userprofile.php");
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,7 +13,7 @@
         body{
             margin: 0;
             padding: 0;
-            background: url(img/bg.png) repeat;
+            background: url(img/clearsky.jpg) repeat;
         }
         #signin{
             margin: 5% auto 3% auto;
@@ -15,6 +22,7 @@
             background-color: #3bd9ef;
             -webkit-border-radius: 5px;
             border-radius: 5px;
+            
 
        }
        input[type=text] {
@@ -24,6 +32,7 @@
             -webkit-border-radius: 5px;
             border-radius: 5px;
             margin: 10px;
+            
        }
        input[type=password]{
             width: 80%;
@@ -53,34 +62,72 @@
             margin: 10px;
             width: 82%;
         }
+            #footer{
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+            }
 
     </style>
     </head>
     <body>
-        
+        <?php include 'header.php';?>
         <div id="signin">
 
-            <form>
-            <table align="center" action="main.php">
+            <form method="post" action="signin.php">
+            <table align="center" >
                 <tr>
                     <td style="color:white;font-family: sans-serif;" align="center">Sign In</td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="email" placeholder="Valid Email" ></td>
+                    <td><input type="text" name="email" placeholder="Email" ></td>
                 </tr>
                 <tr>
                     <td></label><input type="password" name="password" placeholder="Password" ></td>
                 </tr>
                 <tr>
-                    <td align="center"><input type="submit" value="Sign in" ></td>
+                    <td align="center"><input type="submit" value="Sign in" name="onsubmit" ></td>
                 </tr>
                 <tr>
-                    <td align="center"><p style="text-decoration: none;font-family: sans-serif;">If You new Then Register First</p></td>
+                    <td align="center"><p style="text-decoration: none;font-family: sans-serif;">If You new Then <a href="signup.php">Register</a></p></td>
+                </tr>
+                <tr>
+                    <td>
+                                        <?php
+                        include 'includes/dbconnection.php';
+                        
+                        if(isset($_POST['onsubmit'])){
+                            
+                            $email =$_POST['email'];
+                            $password= $_POST['password'];
+                            
+                             $check_user="SELECT * FROM signup WHERE email='$email' AND password='$password'";
+                             
+                             $run1=mysqli_query($link,$check_user);
+                             
+                             
+                         //    $sql="SELECT first_name FROM signup WHERE email='$email' limit 1";
+                        //   $result = mysqli_query($link,$sql);
+                           // $value = mysqli_fetch_field($result);
+                          //$row = mysqli_affected_rows($result);
+                             
+                             if(mysqli_num_rows($run1)>0){
+                                  echo "<script>window.open('userprofile.php','_self')</script>"; 
+                                 $_SESSION['email']=$email;
+                              //   $_SESSION['user_name']=$result;
+                             }
+ else {
+                                 echo "<span  style='color:red; text-align: center;'>wrong email and password</span>";
+ }
+                        }
+                        
+                ?>
+
+                    </td>
                 </tr>
             </table>
             </form>
         </div>
-
+ <div id="footer" > <?php include 'includes/footer.php';?> </div>
     </body>
 </html>
-
